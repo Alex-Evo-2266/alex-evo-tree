3ff67ebf7385c56cc694e540f53a74d24ca85cec
Alex-Evo-2266 <ghbdtn2266@mail.ru>
2026-07-04 21:53:08 +0300

    trash

--- src/lib/components/TreeTrashZone.tsx
+++ src/lib/components/TreeTrashZone.tsx
@@ -1,7 +1,8 @@
-import { useDroppable } from "@dnd-kit/core";
+import { useDndContext, useDroppable } from "@dnd-kit/core";
 
 export function TreeTrashZone() {
 
+    const {active} = useDndContext()
     const { setNodeRef, isOver } = useDroppable({
         id: "trash",
         data: {
@@ -14,9 +15,15 @@ export function TreeTrashZone() {
     return (
         <div
             ref={setNodeRef}
-            className={`tree-trash-zone ${
-                isOver ? "active" : ""
-            }`}
+            className={`
+                tree-trash-zone 
+                ${
+                    isOver ? "active" : ""
+                }
+                ${
+                    !!active ? "visible" : ""
+                }
+                `}
         >
             🗑 Drop here to delete
         </div>
